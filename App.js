01c94dee4ee433c7d9951d784e30b3995d033573
MD GULAM SARWAR <120262909+gulamsarwar55@users.noreply.github.com>
2024-09-01 22:44:39 +0530

    Add files via upload
    /home/gulam/mobile/only coding/my_assesment1/Link to index.html

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import News from "./components/news/News";
+
+const App =()=>{
+  return(
+    <>
+    <Navbar></Navbar>
+    <News></News>
+
+
+    </>
+  )
+}
+
+export default App
